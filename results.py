03bb5e1ea308a1fbb719f5fea0e823c7203949a3
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-03-25 08:58:25 +0100

    Lists

---
+++ results.py
@@ -0,0 +1,15 @@
+# results = ["Mario", "Luigi"]
+
+# results.append("Princess Peach")
+# results.append("Yoshi")
+# results.append("Koopa Troopa")
+# results.append("Toad")
+# results.extend(["Daisy", "Bowser"])
+
+# print(results)
+
+results = ["Mario", "Luigi", "Princess", "Yoshi", "Koopa Troopa", "Toad", "Bowser", "Donkey Kong Jr."]
+results.remove("Bowser")
+results.insert(0, "Bowser")
+results.reverse()
+print(results)
