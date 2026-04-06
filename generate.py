3787baf1ef3f3c27425fafe410742dd33d0414e5
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-04-06 15:18:49 +0200

    Libraries

---
+++ generate.py
@@ -0,0 +1,14 @@
+# from random import choice
+# coin = choice(["heads", "tails"])
+# print(coin)
+
+# import random
+# num = random.randint(1, 10)
+# print(num)
+
+# import random
+# cards = ["Jack", "Queen", "King"]
+# random.shuffle(cards)
+# for card in cards:
+    # print(card)
+
