25ea2637642a2f01374f7205237198a9d3bd840e
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-04-15 16:23:56 +0200

    Calculator

--- calculator.py
+++ calculator.py
@@ -5,12 +5,12 @@
 #print(f"Answer: {x / y:.2f}")
 #print(int(input("Enter x: ")) + int(input("Enter y: ")))
 def main():
-    x = int(input("Enter value of x: "))
+    x = input("Enter value of x: ")
     print("x sqaured is", square(x))
     print("x divided by 2 is", half(x))
 
 def square(n):
-    return pow(n, 2)   #can use n * n or n**2
+    return n * n   #can use n * n or n**2 or pow(n, 2)
 
 def half(n):
     return n / 2  
