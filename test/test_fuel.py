8a426ac9bf0a260876b33fcbadce50acd1d29980
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-04-19 16:15:26 +0200

    Modified Fuel.py file

---
+++ test/test_fuel.py
@@ -0,0 +1,15 @@
+import pytest
+from fuel2 import convert
+from fuel2 import gauge
+
+def test_convert():
+    assert convert("1/2") == 50
+    with pytest.raises(ValueError):
+        convert("3/2")
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+def test_gauge():
+    assert gauge(1) == "E"
+    assert gauge(99) == "F"
+    assert gauge(50) == "50"
