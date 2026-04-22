16340e58fee118d99e73ed5d680145526cb7f5fb
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-04-22 20:50:35 +0200

    File I/O

---
+++ file_io/names_read.py
@@ -0,0 +1,13 @@
+
+#with open("names.txt") as file:
+    #for line in sorted(file):
+        #"print("hello,",line.rstrip())
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
