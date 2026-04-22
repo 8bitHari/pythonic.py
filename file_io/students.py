bc7e33073e59a9b4b0e5e2b4e3b94587fd408ecd
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-04-22 20:50:35 +0200

    File I/O

--- file_io/students.py
+++ file_io/students.py
@@ -26,12 +26,5 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
-
-
-def get_name(student):
-    return student["name"]
-
-
-
-for student in sorted (students, key=get_name, reverse=True):
+for student in sorted (students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
