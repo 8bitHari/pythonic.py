aed5296b753e6470e6d16a9a3281930a77460e46
Harinath Rao <hari@Harinaths-MacBook-Air.local>
2026-04-26 17:23:07 +0200

    CS50 Problem set

---
+++ cs50_shirt/shirt.py
@@ -0,0 +1,30 @@
+import sys
+from PIL import Image
+from PIL import ImageOps
+
+def main():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    if not input_file.endswith((".jpg", ".jpeg", ".png")):
+        sys.exit("Invalid input")
+    if not output_file.endswith((".jpg", ".jpeg", ".png")):
+        sys.exit("Invalid output")
+    if input_file.split(".")[-1] != output_file.split(".")[-1]:
+        sys.exit("Input and output have different extensions")
+    
+    
+    try:
+        input_img = Image.open(input_file)
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+    shirt_img = Image.open("shirt.png")
+    size = input_img.size
+    shirt_img = ImageOps.fit(shirt_img, size)
+    input_img.paste(shirt_img, shirt_img)
+    input_img.save(output_file)
+
+main()
